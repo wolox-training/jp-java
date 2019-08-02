d8e1de64926fb2328094e3bfc3bdfe6f3d07b280
Jhovanny Canas <jhovanycanas@Woloxs-MacBook-Pro.local>
2019-08-02 14:26:39 -0500

    add devtools

--- training/src/main/java/com/wolox/training/TrainingApplication.java
+++ training/src/main/java/com/wolox/training/TrainingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrainingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainingApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TrainingApplication.class, args);
+  }
 }
