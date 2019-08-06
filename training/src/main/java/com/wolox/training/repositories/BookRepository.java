37605f8f0774216370e2670d1447500aac85ec96
Jhovanny Canas <jhovanycanas@Woloxs-MacBook-Pro.local>
2019-08-06 13:49:07 -0500

    optional safe

--- training/src/main/java/com/wolox/training/repositories/BookRepository.java
+++ training/src/main/java/com/wolox/training/repositories/BookRepository.java
@@ -1,9 +1,10 @@
 package com.wolox.training.repositories;
 
 import com.wolox.training.models.Book;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-  Book findFirstByAuthor(String author);
+  Optional<Book> findFirstByAuthor(String author);
 }
