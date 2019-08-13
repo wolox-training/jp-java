73eda51149c31137b5537b123c933143b9bf78e2
Jhovanny Canas <jhovanycanas@Woloxs-MacBook-Pro.local>
2019-08-13 10:31:02 -0500

    remove unnecessary imports

--- training/src/main/java/com/wolox/training/models/User.java
+++ training/src/main/java/com/wolox/training/models/User.java
@@ -13,14 +13,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
